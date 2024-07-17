88743e87657d4ef2af40fa75df7ac0406c03a4f3
datapirate28 <hashmatullah.naybi28@gmail.com>
2024-07-17 08:23:08 +0300

    completed the app baby

--- daily-tasks-tracker/app.js
+++ daily-tasks-tracker/app.js
@@ -16,12 +16,24 @@ function addTask(){
         li.appendChild(span)
     }
     inputBox.value = ''; 
+    savaData();
 }
 
 listContainer.addEventListener('click', function(e){
     if(e.target.tagName === 'LI'){
         e.target.classList.toggle('checked');
+        savaData();
     }else if(e.target.tagName === 'SPAN'){
         e.target.parentElement.remove();
+        savaData();
     }
 }, false);
+
+function savaData(){
+    localStorage.setItem('data', listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+showTask()
