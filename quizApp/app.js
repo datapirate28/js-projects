a76a4d0ef62d38e09b28ac9d7f92e30067b9b6ce
datapirate28 <hashmatullah.naybi28@gmail.com>
2024-08-31 16:18:35 +0300

    stopped workiing on this since it was a bit advanced for me. I will come back.

--- quizApp/app.js
+++ quizApp/app.js
@@ -1,7 +1,7 @@
 const questions = [
     {
         question: 'Which of the following is the largest desert in the world by area?',
-        answer: [
+        answers: [
             { text: 'Gobi Desert', correct: false}, 
             { text: 'Sahara Desert', correct: false}, 
             { text: 'Arabian Desert', correct: false}, 
@@ -10,7 +10,7 @@ const questions = [
     },
     {
         question: 'Which country is home to the worlds tallest waterfall, Angel Falls?',
-        answer: [
+        answers: [
             { text: 'Brazil', correct: false}, 
             { text: 'Venezuela', correct: true}, 
             { text: 'Peru', correct: false}, 
@@ -19,7 +19,7 @@ const questions = [
     },
     {
         question: 'Which of these cities is NOT located on the Mediterranean Sea?',
-        answer: [
+        answers: [
             { text: 'Barcelona', correct: false}, 
             { text: 'Lisbon', correct: true}, 
             { text: 'Naples', correct: false}, 
@@ -31,3 +31,53 @@ const questions = [
 const questionElement = document.querySelector('#question');
 const answerBtn = document.querySelector('#answer-buttons')
 const nextBtn = document.querySelector('#next-btn')
+
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextBtn.innterText = 'Next';
+    showQuestion()
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex]
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerText = `${questionNo}. ${currentQuestion.question}`
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerHTML = answer.text;
+        button.classList.add('btn');
+        answerBtn.append(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('clikc', selectAnswer);
+    })
+}
+
+
+function resetState(){
+    nextBtn.style.display = 'none';
+    while(answerBtn.firstChild){
+        answerBtn.removeChild(answerBtn.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === 'true';
+    if(isCorrect){
+        selectedBtn.classList.add('correct');
+    }else{
+        selectedBtn.classList.add('incorrect');
+    }
+}
+
+startQuiz();
+
